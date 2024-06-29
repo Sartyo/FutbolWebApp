6fa6ba417ee230f2fc95b4846ff327baaca5d12b
Daniel <danielarv14121999@gmail.com>
2024-06-29 11:48:23 -0400

    signUp form updated to save first and last name of user

--- FutbolWebApp/authenticator/forms.py
+++ FutbolWebApp/authenticator/forms.py
@@ -9,4 +9,4 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
